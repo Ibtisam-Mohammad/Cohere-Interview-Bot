09510fe426d60e27dec8d5e1f972f8ce03dd381c
Ibtisam-Mohammad <63063432+Ibtisam-Mohammad@users.noreply.github.com>
2023-02-04 04:06:43 +0530

    Update cohere_hackaton.py

--- cohere_hackaton.py
+++ cohere_hackaton.py
@@ -43,7 +43,7 @@ uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 input_role = st.text_input(label='Give the role you are applying for',key="input_role")
 def calculate_similarity(a, b):
         return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-def correctness_check(question,answer,role = ROLE):
+def correctness_check(question,answer,role = input_role):
       text_list=[]
       for i in range(3):
         definitions = co.generate(
@@ -88,10 +88,9 @@ if (uploaded_file is not None) and (input_role!=''):
 
   # Resume/Behavioral questions
     NUM_BEHAVIORAL_QUESTION=3
-    ROLE='Machine Learning'
     response_behaviour_q = co.generate(
       model='command-xlarge-nightly',
-      prompt=f'''Following text is my resume:\n"{response_resume.generations[0].text}"\nI am preparing for a behavioral interview for a {ROLE} role. Generate {str(NUM_BEHAVIORAL_QUESTION)} questions from my resume related to my education, work experience, projects:''',
+      prompt=f'''Following text is my resume:\n"{response_resume.generations[0].text}"\nI am preparing for a behavioral interview for a {input_role} role. Generate {str(NUM_BEHAVIORAL_QUESTION)} questions from my resume related to my education, work experience, projects:''',
       max_tokens=300,
       temperature=1,
       k=0,
@@ -106,7 +105,7 @@ if (uploaded_file is not None) and (input_role!=''):
     NUM_TECHNICAL_QUESTION=7
     response_technical_q = co.generate(
       model='command-xlarge-nightly',
-      prompt=f'Data Science: Explain the difference between Linear Regression and Logistic Resression.\nSystem Design: Design a distributed file system on top of commodity hardware.\nDatabase: How do you design a database schema to store user activity?\nNetworking: How does the TCP handshake work?\nLinux: What is the difference between hard links and symbolic links?\nC: What are some limitations of malloc()/free()?\nPython: What is the difference between .__getitem__() and .[]?\nCSS: How do you vertically center an element using CSS?\nR: What is the RStudio IDE?\nSalesforce: What is the difference between Workflow, Process Builder, and Apex?\nMongoDB: How would you model a blog in MongoDB?\nSDE: How would you design a REST API for a social media app?\nQA: How do you write a test case for Google Maps?\nDigital Marketing: How do you measure the success of a digital marketing campaign?\nJAVA: What is the difference between checked and unchecked exceptions?\nDevOps: How do you configure a production environment on AWS?\nGit: How do you resolve merge conflicts in Git?\nFollowing the above format, write {str(NUM_TECHNICAL_QUESTION)} of most diverse and logical technical {ROLE} interview questions without answers from Geeks for Geeks:',
+      prompt=f'Data Science: Explain the difference between Linear Regression and Logistic Resression.\nSystem Design: Design a distributed file system on top of commodity hardware.\nDatabase: How do you design a database schema to store user activity?\nNetworking: How does the TCP handshake work?\nLinux: What is the difference between hard links and symbolic links?\nC: What are some limitations of malloc()/free()?\nPython: What is the difference between .__getitem__() and .[]?\nCSS: How do you vertically center an element using CSS?\nR: What is the RStudio IDE?\nSalesforce: What is the difference between Workflow, Process Builder, and Apex?\nMongoDB: How would you model a blog in MongoDB?\nSDE: How would you design a REST API for a social media app?\nQA: How do you write a test case for Google Maps?\nDigital Marketing: How do you measure the success of a digital marketing campaign?\nJAVA: What is the difference between checked and unchecked exceptions?\nDevOps: How do you configure a production environment on AWS?\nGit: How do you resolve merge conflicts in Git?\nFollowing the above format, write {str(NUM_TECHNICAL_QUESTION)} of most diverse and logical technical {input_role} interview questions without answers from Geeks for Geeks:',
       max_tokens=300,
       temperature=1.8,
       k=0,
@@ -166,7 +165,7 @@ if (uploaded_file is not None) and (input_role!=''):
 
               
               if st.session_state['iterator']>2:
-                st.session_state['correctness_list'].append(correctness_check(question = bot,answer = user, role = ROLE))
+                st.session_state['correctness_list'].append(correctness_check(question = bot,answer = user, role = input_role))
               st.session_state['iterator']+=1
 
               if user=='stop':
@@ -185,4 +184,3 @@ if (uploaded_file is not None) and (input_role!=''):
       for i in range(len(st.session_state['generated'])-1, -1, -1):  # Print messages in reverse order of a list i.e newest to oldest
           message(st.session_state["generated"][i], key=str(i))      # Print BOT messages
           message(st.session_state['past'][i], is_user=True, key=str(i) + '_user') # Print USER messages
-
